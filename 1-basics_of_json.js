ba9be8eb90af4cf9e3e271838d1315dee5e4f57a
Vivek Kurhe <vivek.kurhe@launchventures.co>
2022-10-17 23:39:46 +0530

    json learnings

---
+++ 1-basics_of_json.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * 
+ * key/name value pairs
+ * {"name":"value"}
+ * 
+ * Objects are comma separated
+ * {"name1":"value", "name2":"value", "name3":"value"}
+ * 
+ * Arrays have square brackets with values separated by comma
+ * {"name":[{"name":"value"}, {"name":"value"}]}
+ * 
+ * 
+ * 
+ * 
+ */
---
+++ 2-json_as_data.js
@@ -0,0 +1,22 @@
+/**
+ * Difference between JSON and Javascript Object
+ * 
+ * JSON all keys must be quoted, object literals it is not necessary
+ * {"foo":"bar"}
+ * 
+ * var o = {foo:"bar"};
+ * 
+ * JSON has double quotes while javascript can use single or double quotes
+ * 
+ * JavaScript can include functions which is not available in JSON.
+ * 
+ * 
+ * Different data types in json
+ * 
+ * String
+ * Number
+ * Boolean
+ * Object
+ * Array of objects
+ * Null
+ */
